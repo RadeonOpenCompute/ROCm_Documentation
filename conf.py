ba717ef8f1f09c04fe7fcfc43d33f3bb1d005633
srihari.karnam <srihari.karnam@tcs.com>
2019-01-24 14:39:43 +0530

    includes_HIP_API_documentation

--- conf.py
+++ conf.py
@@ -44,7 +44,7 @@ extensions = [
 ]
 
 # Breathe extension variables
-breathe_projects = { "ReadTheDocs-Breathe": "/ROCm_API_References/HIP_Docs/doxyxml/" }
+breathe_projects = { "ReadTheDocs-Breathe": "xml/" }
 breathe_default_project = "ReadTheDocs-Breathe"
 
 # Add any paths that contain templates here, relative to this directory.
