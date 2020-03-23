f68f8f84968b35406913bdabac8b73a76f76803e
Roopa Malavally <56051583+Rmalavally@users.noreply.github.com>
2020-03-23 14:48:24 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -168,7 +168,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_css_files = [
-'css/amdstyles.css',
+"amdstyles.css",
 ]
 
 # Add any extra paths that contain custom files (such as robots.txt or
